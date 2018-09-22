4379433c2536f681ae0c06de74d00448dc3fdbb0
JieWHuang <34201419+JieWHuang@users.noreply.github.com>
2018-09-22 20:56:21 +0800

    Add animations

--- static/js/articlesListAjax.js
+++ static/js/articlesListAjax.js
@@ -6,22 +6,31 @@ function get_list(url) {
         success: function (data) {
             if (data.data.length != 0) {
                 for (var i = 0; i < data.data.length; i++) {
-                    label_box_html = '';
-                    tags = data.data[i].tag;
-                    for (var j = 0; j < tags.length; j++) {
-                        label_box_html = label_box_html + '<a href="/tag/' + tags[j].tag_id + '" class="label"><i class="fa fa-tag"></i>' + tags[j].tag_name + '</a>'
+                    function put(i) {
+                        label_box_html = '';
+                        tags = data.data[i].tag;
+                        for (var j = 0; j < tags.length; j++) {
+                            label_box_html = label_box_html + '<a href="/tag/' + tags[j].tag_id + '" class="label"><i class="fa fa-tag"></i>' + tags[j].tag_name + '</a>'
+                        }
+                        item_html = '<li class="post_item" id=' + data.data[i].id + '><h3><i class="fa fa-hand-o-right"></i> <a href="article/' + data.data[i].id + '" title="' + data.data[i].title + '" id="title">' + data.data[i].title + '</a></h3><div class="description"><span class="time">' + data.data[i].create_time + ' - </span>' + data.data[i].description + '</div><div class="article_info"><span class="classify"><a href="/category/' + data.data[i].category_id + '/"><i class="fa fa-hashtag"></i>' + data.data[i].category + '</a></span><div class="label_box">' + label_box_html + '</div><span class="view"><i class="fa fa-eye"></i> ' + data.data[i].click_nums + '</span>' + '<a href="article/' + data.data[i].id + '#comment">' + '<span class="reply"><i class="fa fa-comment"></i> ' + data.data[i].cmt_nums + '</span></a></div></li>';
+                        $('#summary_list').append(item_html);
+                        setTimeout(function () {
+                            $('#' + data.data[i].id).addClass("list_show");
+                        }, i * 500);
                     }
-                    item_html = '<li class="post_item" id=' + data.data[i].id + '><h3><i class="fa fa-hand-o-right"></i> <a href="article/' + data.data[i].id + '" title="' + data.data[i].title + '" id="title">' + data.data[i].title + '</a></h3><div class="description"><span class="time">' + data.data[i].create_time + ' - </span>' + data.data[i].description + '</div><div class="article_info"><span class="classify"><a href="/category/' + data.data[i].category_id + '/"><i class="fa fa-hashtag"></i>' + data.data[i].category + '</a></span><div class="label_box">' + label_box_html + '</div><span class="view"><i class="fa fa-eye"></i> ' + data.data[i].click_nums + '</span>' + '<a href="article/' + data.data[i].id + '#comment">' + '<span class="reply"><i class="fa fa-comment"></i> ' + data.data[i].cmt_nums + '</span></a></div></li>';
-                    $('#summary_list').append(item_html);
+                    put(i);
                 }
                 if (data.paging.totals == data.paging.current_page) {
                     $('#more').html("没有更多文章了...")
                 } else {
-                    $('#more').html('<a href="javascript:get_list_btn();" id="next">点击加载更多...</a>')
+                    $('#more').html('<a href="javascript:void(0);" onclick="get_list_btn()" id="next">点击加载更多...</a>')
                 }
             } else {
                 $('#more').html("文章加载失败了...")
             }
+            setTimeout(function () {
+                $('#more').addClass("list_show");
+            }, (data.data.length + 1) * 400);
         }
     });
 }
